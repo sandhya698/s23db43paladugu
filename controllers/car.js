7c20e619a3e747b43be0217e12a6436ee66e4ea0
sandhya paladugu <sandhyachoudhary698@gmail.com>
2023-04-14 13:24:52 -0500

    changes has been done

--- controllers/car.js
+++ controllers/car.js
@@ -30,7 +30,7 @@ exports.car_create_post = async function(req, res) {
  // We are looking for a body, since POST does not have query parameters.
  // Even though bodies can be in many different formats, we will be picky
  // and require that it be a json object
- // {"manufracturer":"goat", "price":12, "size":"large"}
+ // {"manufracturer":"goat", "price":12, "price":"large"}
  document.manufracturer = req.body.manufracturer;
  document.price = req.body.price;
  document.model = req.body.model;
@@ -78,5 +78,25 @@ exports.car_detail = async function(req, res) {
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
     };
-    
+
+    // Handle car update form on PUT.
+exports.car_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await car.findById( req.params.id)
+// Do updates of properties
+if(req.body.manufacturer)
+toUpdate.manufacturer = req.body.manufacturer;
+if(req.body.model) toUpdate.model = req.body.model;
+if(req.body.price) toUpdate.price = req.body.price;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
 
